4d9d1e300c1fe4672e4de85a4fea768d3881607e
tcurtis97 <timcurtis97@yahoo.com>
2021-01-13 13:09:04 -0600

    added login and registration ability

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,3 +1,12 @@
+const contentTarget = document.querySelector(".dashboard")
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("userAuthenticated", e => {
+    contentTarget.innerHTML = Nutshell()
+})
+
+
+
 export const Nutshell = () => {
     // Render all your UI components here
 }
